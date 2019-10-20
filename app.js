a4de5e2951b1be1cb5a06016efd2577870aa7325
Brian Bycynski <high5bri@hotmail.com>
2019-10-20 05:54:29 -0400

    still working

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("app.js");
+//
+//
+
+// Init GitHub
+const github = new Github();
+// Init UI
+const ui = new UI();
+
+// Search Input
+const searchUser = document.querySelector("#searchUser");
+
+// Search Input Event Listener
+searchUser.addEventListener("keyup", e => {
+  // Get Input Text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // Make HTTP Call
+    github.getUser(userText).then(data => {
+      console.log(data);
+      if (data.profile.message === "Not Found") {
+        // Show Alert
+      } else {
+        // Show Profile
+        ui.showProfile(data.profile);
+      }
+    });
+  } else {
+    // Clear Profile
+  }
+});
